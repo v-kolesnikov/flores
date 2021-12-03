fa4e220e2696c35adc60bb1b8b2336712c15a497
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2021-12-04 01:09:48 +0300

    Add sample index page

--- lib/florinda_ctl/controllers/page_controller.ex
+++ lib/florinda_ctl/controllers/page_controller.ex
@@ -2,6 +2,9 @@ defmodule FlorindaCtl.PageController do
   use FlorindaCtl, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    conn
+    |> put_layout(false)
+    |> put_root_layout(false)
+    |> render("index.html")
   end
 end

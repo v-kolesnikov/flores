9c6fc5f6438d3b2deb02cc54c521280c1fe8ef1d
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2021-12-05 03:38:23 +0300

    Improve pagination links

--- lib/florinda_ctl/views/shared_view.ex
+++ lib/florinda_ctl/views/shared_view.ex
@@ -1,3 +1,21 @@
 defmodule FlorindaCtl.SharedView do
   use FlorindaCtl, :view
+
+  def next_page_url(conn, page) do
+    query =
+      conn.query_params
+      |> Map.merge(%{"starting_after" => page.metadata.after})
+      |> Map.delete("ending_before")
+      |> URI.encode_query
+    "#{conn.request_path}?#{query}"
+  end
+
+  def prev_page_url(conn, page) do
+    query =
+      conn.query_params
+      |> Map.merge(%{"ending_before" => page.metadata.before})
+      |> Map.delete("starting_after")
+      |> URI.encode_query
+    "#{conn.request_path}?#{query}"
+  end
 end

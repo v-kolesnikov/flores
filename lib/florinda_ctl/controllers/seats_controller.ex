71a6ce1a0481434faf1dbb2139d857e1cc2ab92a
Vasily Kolesnikov <re.vkolesnikov@gmail.com>
2021-12-06 19:08:31 +0300

    Add pagination to `/seats?aircraft=` page

--- lib/florinda_ctl/controllers/seats_controller.ex
+++ lib/florinda_ctl/controllers/seats_controller.ex
@@ -6,11 +6,19 @@ defmodule FlorindaCtl.SeatsController do
 
   import Ecto.Query
 
-  def index(conn, _params) do
-    seats =
-      Seat
-      |> limit(20)
-      |> Repo.all
-    render(conn, "index.html", seats: seats)
+  def index(conn, params) do
+    aircraft_code = params |> Map.get("aircraft")
+    query = from t in Seat,
+      where: t.aircraft_code == ^aircraft_code,
+      order_by: [desc: t.aircraft_code, desc: t.seat_no]
+    page = Repo.paginate(
+      query,
+      before: params |> Map.get("ending_before"),
+      after: params |> Map.get("starting_after"),
+      cursor_fields: [:aircraft_code, :seat_no],
+      sort_direction: :desc,
+      limit: 20
+    )
+    render(conn, "index.html", page: page)
   end
 end
